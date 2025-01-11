8f8e9aa37e966c7ae2d49246979f4443caf801e5
Gedion-01 <117908662+Gedion-01@users.noreply.github.com>
2025-01-11 15:45:09 +0300

    added an authcontext + hook for auth

---
+++ Frontend/ecommerce-app-frontend/contexts/AuthContext.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import React, { createContext, useContext, useState, useEffect } from "react";
+import Cookies from "js-cookie";
+
+interface AuthContextType {
+  isAuthenticated: boolean;
+  login: (token: string) => void;
+  logout: () => void;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const token = Cookies.get("token");
+    setIsAuthenticated(!!token);
+  }, []);
+
+  const login = (token: string) => {
+    Cookies.set("token", token);
+    setIsAuthenticated(true);
+  };
+
+  const logout = () => {
+    Cookies.remove("token");
+    setIsAuthenticated(false);
+  };
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+};
